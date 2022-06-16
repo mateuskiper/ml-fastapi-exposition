e00b145a1e5b582f0c002f40d43cd8614218a395
mateuskiper <mateuseduardokiper@gmail.com>
2022-06-16 10:08:19 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="ml-fastapi-exposition",
+    version="0.0.1",
+    description="Code for Udacity about ML Heroku deployment with FastAPI.",
+    author="Mateus Kiper",
+)
